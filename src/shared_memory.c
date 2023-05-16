d87e05e58ae6a73088c2dffe1688c8c1dc65ca43
Marica Bottega <bottegamarica02@gmail.com>
2023-05-16 12:44:53 +0200

    DIOPORCO

--- src/shared_memory.c
+++ src/shared_memory.c
@@ -1,2 +1,12 @@
 #include <sys/shm.h>
 #include <sys/stat.h>
+
+struct tavolo_da_gioco{
+    int nRighe;
+    int nColonne;
+    /*
+    char param1;
+    char param2;
+    char **griglia;
+    */
+};
