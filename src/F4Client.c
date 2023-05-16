2eef1b539cf30eb5be4e88164d6bc5a3a45e313b
Marica Bottega <bottegamarica02@gmail.com>
2023-05-16 10:02:01 +0200

    prova di stampa della matrice con mem condivisa

--- src/F4Client.c
+++ src/F4Client.c
@@ -5,13 +5,12 @@
 #include <sys/shm.h>
 #include <sys/stat.h>
 
-//#include "shared_memory.h"
-//struct tavolo_da_gioco tavolo;
+#include "shared_memory.h"
+struct tavolo_da_gioco tavolo;
 
 void stampaMatrice(int nRighe, int nColonne){ 
 //stampa la matrice ad ogni turno di gioco (aggiornata)
 // la deve costruire secondo la mem condivisa
-
     char matrice[nRighe][nColonne];
     //riempimento matrice;
     for(int r = 0; r < nRighe; r++){
@@ -36,21 +35,22 @@ void stampaMatrice(int nRighe, int nColonne){
 }
 
 int main(int argc, char * argv[]){
-    /*int nRighe;
+    int nRighe;
     int nColonne;
     char *param1, *param2;
-    */
+    
     ssize_t sizeMem = sizeof(struct tavolo_da_gioco);
     key_t chiave = 3945;//atoi(argv[0]); //chiave generata a mano
     int shmTavId = shmget(chiave, sizeMem, IPC_CREAT | S_IRUSR | S_IWUSR);
     if(shmTavId == -1)
         printf("Errore shmget\n");
-
+    
     //allaccio memoria
-    /*
+    
     struct tavolo_da_gioco *allaccio = (struct tavolo_da_gioco *)shmat(shmTavId, NULL, 0);
     
     printf("%d\n", allaccio->nColonne);
     printf("%d\n", allaccio->nRighe);
-    */
+    stampaMatrice(allaccio->nRighe, allaccio->nColonne);
+    
 }
